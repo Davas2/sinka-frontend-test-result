76f3a59627173063718531d25cc5c24e1ecb5ad4
Davas2 <daviperes00@gmail.com>
2025-02-15 10:35:37 -0300

    Fixing title

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   return (
     <div className="page-wrapper">
       <div className="container shadow-xl bg-background p-8 rounded-xl border border-gray-700">
-        <h1 className="text-white text-4xl font-bold mb-6 text-center">Gerenciamento de Clientees</h1>
+        <h1 className="text-white text-4xl font-bold mb-6 text-center">Gerenciamento de Clientes</h1>
         <ClientsPage />
       </div>
     </div>
